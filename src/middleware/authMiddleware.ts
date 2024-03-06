c281b9a4c0b9be274d71b7a82544a1bd648dfd92
Arkansiel92 <yaj92250@hotmail.fr>
2024-03-06 22:50:25 +0100

    auth middleware added

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import AuthService from "../services/authService";
+
+export default function authMiddleware(req: Request, res: Response, next: NextFunction) {
+    const authService = new AuthService();
+    const token = req.header('Autorization');
+
+    if(!token) throw new Error('Token not found. Please try to connect.');
+
+    if(token.split(' ')[0] !== 'Bearer') throw new Error('Invalid token format');
+
+    try {
+        const decoded = authService.verifyToken(token.split(' ')[1]);
+        next();
+    } catch (error) {
+        throw new Error('Token is not valid, try to reconnect');
+    }
+}
